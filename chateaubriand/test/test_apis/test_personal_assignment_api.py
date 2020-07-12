838d0a481703cf9dc8c30afbe86d92a00ef935de
Maly <migsking@naver.com>
2020-07-12 21:25:02 +0900

    [MODIFY] <**/test_personal_assignment_api.py> mock

--- chateaubriand/test/test_apis/test_personal_assignment_api.py
+++ chateaubriand/test/test_apis/test_personal_assignment_api.py
@@ -1,12 +1,12 @@
 import os
-import jwt
 
 from test import BaseTestCase
+from test.test_apis.mocks import jwt_mock
 
 
 class PersonalAssignmentTestCase(BaseTestCase):
     def test_get(self):
-        token = jwt.encode({},self.app.config["SECRET_KEY"] ,algorithm="HS256" )
+        token = jwt_mock(self.app)
 
         resp = self.test_client.get('/personal-assignment', 
         json = {"class": 1}, 
