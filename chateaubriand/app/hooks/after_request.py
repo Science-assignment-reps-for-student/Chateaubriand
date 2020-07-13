254e7937d5eb47cfd61dbfa4e83289e38ddf55c5
Maly <migsking@naver.com>
2020-07-13 19:26:21 +0900

    [MODIFY] <app/extensions, app/hooks/after_request> 로그 형식 변경

--- chateaubriand/app/hooks/after_request.py
+++ chateaubriand/app/hooks/after_request.py
@@ -2,12 +2,13 @@ import logging
 
 from flask import Response, request
 
+from chateaubriand.app.extensions import logger
+
 
 def after_request(response: Response) -> Response:
     try:
         response.headers["X-Content-Type-Options"] = "nosniff"
         response.headers["X-Frame-Options"] = "deny"
-        logger = logging.getLogger('waitress')
-        logger.info(f"{request.remote_addr}, {request.path}, {request.method}, {response.status_code}")
+        logger.info(f"{request.remote_addr} - [{request.method} {request.path}] {response.status_code}")
     finally:
         return response
