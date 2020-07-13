a00ce646ed5488e69d701f9ba619c6b5111d16f8
Maly <migsking@naver.com>
2020-07-13 15:04:54 +0900

    [ADD] <test/**> 상태 코드 테스트 추가 및 Path 통합

--- chateaubriand/test/test_apis/test_team_assignment_api.py
+++ chateaubriand/test/test_apis/test_team_assignment_api.py
@@ -7,9 +7,19 @@ from test.test_apis.mocks import jwt_mock
 class TeamAssignmentTestCase(BaseTestCase):
     def test_get(self):
         token = jwt_mock(self.app)
+        path = "/team-assignment"
 
-        resp = self.test_client.get('/team-assignment', 
+        resp_200 = self.test_client.get(path, 
         json = {"class": 1}, 
         headers={"authorization": token})
 
-        self.assertEqual(resp.status_code, 200)
+        resp_401 = self.test_client.get(path, 
+        json = {"class": 1})
+
+        resp_406 = self.test_client.get(path, 
+        json = {"class": "1"}, 
+        headers={"authorization": token})
+
+        self.assertEqual(resp_200.status_code, 200)
+        self.assertEqual(resp_401.status_code, 401)
+        self.assertEqual(resp_406.status_code, 406)
