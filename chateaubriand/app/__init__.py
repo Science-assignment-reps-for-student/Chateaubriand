33c74d38e759f2f917908773a3c90b6828f9d058
Maly <migsking@naver.com>
2020-07-13 13:59:28 +0900

    [ADD] <chateaubriand/app> add the config setter in app factory

--- chateaubriand/app/__init__.py
+++ chateaubriand/app/__init__.py
@@ -13,6 +13,14 @@ def register_extensions(app: Flask):
     pass
 
 
-def create_app():
+def create_app(*config_cls) -> Flask:
     app = Flask()
+
+    for config in config_cls:
+        flask_app.config.from_object(config)
+    
+    register_hooks(app)
+    register_controllers(app)
+    register_extensions(app)
+
     return app
