f0e251dc28c4bcc2e4636e5616405359c6f5938c
Maly <migsking@naver.com>
2020-07-13 14:56:51 +0900

    [ADD] <chaateaubriand/config/db_config.py> build

--- chateaubriand/config/db_config.py
+++ chateaubriand/config/db_config.py
@@ -1,15 +1,15 @@
 import os
 
 
+def build_database_uri(username, password, host):
+    return "mysql://" + username + ":" + password + "@" + host
+
+
 class LocalDBConfig:
     DATABASE_USERNAME = os.getenv("LOCAL_DATABASE_USERNAME")
     DATABASE_PASSWORD = os.getenv("LOCAL_DATABASE_PASSWORD")
     DATABASE_HOST = os.getenv("LOCAL_DATABASE_HOST")
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql://root:"
-        + DB_PASSWORD
-        + "@scarfs.cm63idi6gyr1.ap-northeast-2.rds.amazonaws.com/scarfs_production"
-    )
+    SQLALCHEMY_DATABASE_URI = build_database_uri(DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -17,9 +17,5 @@ class RemoteDBConfig:
     DATABASE_USERNAME = os.getenv("DATABASE_USERNAME")
     DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
     DATABASE_HOST = os.getenv("DATABASE_HOST")
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql://root:"
-        + DB_PASSWORD
-        + "@scarfs.cm63idi6gyr1.ap-northeast-2.rds.amazonaws.com/scarfs_production"
-    )
+    SQLALCHEMY_DATABASE_URI = build_database_uri(DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
