17579ae0185363f300dd1aa931ad5a11042a3168
Maly <migsking@naver.com>
2020-07-14 22:00:56 +0900

    [MODIFY] <app/models/single_file.py> Add the Foreign Key

--- chateaubriand/app/models/single_file.py
+++ chateaubriand/app/models/single_file.py
@@ -5,8 +5,8 @@ class SingleFileModel(db.Model):
     __tablename__ = "single_file"
 
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer)
-    homework_id = db.Column(db.Integer)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    homework_id = db.Column(db.Integer, db.ForeignKey("homework.id"))
     file_name = db.Column(db.String, nullable=False)
     path = db.Column(db.String, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
