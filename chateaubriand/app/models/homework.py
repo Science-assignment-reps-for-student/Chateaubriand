870849a092bca2670838e528451fc5ba4006a495
Maly <migsking@naver.com>
2020-07-14 20:20:24 +0900

    [CREATE] <app/models/homework.py> create homework model

---
+++ chateaubriand/app/models/homework.py
@@ -0,0 +1,15 @@
+from chateaubriand.app.extensions import db
+
+
+class HomeworkModel(db.Model):
+    __tablename__ = "homework"
+
+    id = db.Column(db.Integer, primary_key=True)
+    deadline_1 = db.Column(db.DateTime, nullable=False)
+    deadline_2 = db.Column(db.DateTime, nullable=False)
+    deadline_3 = db.Column(db.DateTime, nullable=False)
+    deadline_4 = db.Column(db.DateTime, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    type = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
