231a7f55973d7df030f215ecaf4db5b3fc6c8fda
Maly <migsking@naver.com>
2020-07-14 21:50:51 +0900

    [MODIFY] <app/views> modify view interface (exist self)

--- chateaubriand/app/views/__init__.py
+++ chateaubriand/app/views/__init__.py
@@ -3,10 +3,10 @@ from abc import ABCMeta, abstractmethod
 
 class BaseView(metaclass=ABCMeta):
     @abstractmethod
-    def get_data():
+    def get_data(self):
         pass
     
     @abstractmethod
-    def get_view():
+    def get_view(self):
         pass
     
