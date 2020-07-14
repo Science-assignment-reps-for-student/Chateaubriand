9e53c7b8b1a30a414faab6beef2ff6c23f6ad1d3
Maly <migsking@naver.com>
2020-07-14 17:07:45 +0900

    [CREATE] <app/controller/admin/personal_assignment.py> cr pa controller

--- chateaubriand/app/controllers/admin/personal_assignment.py
+++ chateaubriand/app/controllers/admin/personal_assignment.py
@@ -1,3 +1,13 @@
 from flask import Flask, Blueprint, request, abort
+from flask_restful import Resource
+
+from chateaubriand.app.views.admin.personal_assignment import PersonalAssignmentView
+from chateaubriand.app.util.json_checker import json_type_validate, GET_ASSIGNMENT_JSON
 
 
+class PersonalAssignment(Resource):
+    @json_type_validate(GET_ASSIGNMENT_JSON)
+    def get(self):
+        personal_assignment_view = PersonalAssignmentView(request.json["class"])
+        view = personal_assignment_view.get_view()
+        return view
