da184aaa2e865e8d2eb77da59bf9eed4516be966
Maly <migsking@naver.com>
2020-07-16 19:09:15 +0900

    [MODIFY] <test/test_apis/mocks.py>

--- chateaubriand/test/test_apis/mocks.py
+++ chateaubriand/test/test_apis/mocks.py
@@ -1,12 +1,13 @@
 import jwt
 
 
-def payload_builder(payload, user_type, token_type):
-    payload["sub"] = "test@test.com"
+def payload_builder(payload, user_type, token_type, invalid):
+    if invalid: payload["sub"] = "invalid@test.com"
+    else: payload["sub"] = "test@test.com"
     payload["type"] = token_type
     if user_type == "ADMIN": payload["admin"] = "true"
     return payload
 
-def jwt_mock(app, user_type, token_type):
-    payload = payload_builder({}, user_type, token_type)
+def jwt_mock(app, user_type, token_type, invalid=False):
+    payload = payload_builder({}, user_type, token_type, invalid)
     return jwt.encode(payload=payload, key=app.config["SECRET_KEY"], algorithm="HS256")
