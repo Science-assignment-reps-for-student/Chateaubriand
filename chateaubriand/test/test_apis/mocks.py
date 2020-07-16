dba0d056c9892e7e4e1ce1d1931f287976c52b92
Maly <migsking@naver.com>
2020-07-16 18:33:41 +0900

    [MODIFY] <test/test_apis/mocks.py> token builder

--- chateaubriand/test/test_apis/mocks.py
+++ chateaubriand/test/test_apis/mocks.py
@@ -1,8 +1,12 @@
 import jwt
 
 
-def jwt_mock(app, type):
-    if type == "ADMIN":
-        return jwt.encode({}, app.config["SECRET_KEY"], algorithm="HS256")
-    elif type == "STUDENT":
-        return jwt.encode({}, app.config["SECRET_KEY"], algorithm="HS256")
+def payload_builder(payload, user_type, token_type):
+    payload["sub"] = "test@test.com"
+    payload["type"] = token_type
+    if user_type == "ADMIN": payload["admin"] = "true"
+    return payload
+
+def jwt_mock(app, user_type, token_type):
+    payload = payload_builder({}, user_type, token_type)
+    return jwt.encode(payload=payload, key=app.config["SECRET_KEY"], algorithm="HS256")
