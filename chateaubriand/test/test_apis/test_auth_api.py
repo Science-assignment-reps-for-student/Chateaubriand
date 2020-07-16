8b1946abb20cd5ee4c3fd2a37cb2d4047629a2db
Maly <migsking@naver.com>
2020-07-16 18:49:11 +0900

    [MODIFY] <test/test_apis/test_auth_api.py>
    json form 분리, setUp 설정

--- chateaubriand/test/test_apis/test_auth_api.py
+++ chateaubriand/test/test_apis/test_auth_api.py
@@ -5,14 +5,48 @@ from test.test_apis.mocks import jwt_mock
 
 
 class AuthTestCase(BaseTestCase):
-    def test_post(self):
-        path = "/admin/auth"
+    def __init__(self):
+        self.path = "/admin/auth"
+        self.common_body_post = {
+            "email": "test@test.com",
+            "password": "password"
+        }
 
-        resp_200 = self.test_client.post(
-            path, json={"email": "admin@mallycrip.com", "password": "p@ssw0rd",}
+        self.invalid_body_post = {
+            "email": 123
+        }
+
+        self.invalid_info_body_post = {
+            "email": "test@test.com",
+            "password": "wrong_password"
+        }
+
+    def setUp(self):
+        self.test_client.post(
+            "admin/account",
+            json = {
+                "email": "test@test.com",
+                "password": "password",
+                "name": "테스트",
+            }
         )
 
-        resp_400 = self.test_client.post(path, json={"email": 123})
+    def test_post(self):
+        resp_200 = self.test_client.post(
+            self.path,
+            json = self.common_body_post
+        )
+
+        resp_400 = self.test_client.post(
+            self.path,
+            json = self.invalid_body_post
+        )
+
+        resp_401 = self.test_client.post(
+            self.path,
+            json = self.invalid_info_body_post
+        )
 
         self.assertEqual(resp_200.status_code, 200)
         self.assertEqual(resp_400.status_code, 400)
+        self.assertEqual(resp_401.status_code, 401)
