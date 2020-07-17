bff5adeb8740af483a838a18e0c45b2e6e8bbda6
Maly <migsking@naver.com>
2020-07-17 20:14:50 +0900

    [MODIFY] <controller/admin/auth.py>
    컨트롤러 수정. Account -> auth

--- chateaubriand/app/controllers/admin/auth.py
+++ chateaubriand/app/controllers/admin/auth.py
@@ -2,13 +2,13 @@ from flask import request
 from flask_restful import Resource
 
 from chateaubriand.app.services.admin.auth import AuthService
-from chateaubriand.app.views.admin.account import AccountView
+from chateaubriand.app.views.admin.auth import AuthView
 from chateaubriand.app.util.json_checker import json_type_validate, POST_AUTH_JSON
 
 class Auth(Resource):
     @json_type_validate(POST_AUTH_JSON)
     def post(self):
         AuthService.login(request.json["email"], request.json["password"])
-        auth_view = AccountView(request.json["email"])
+        auth_view = AuthView(request.json["email"])
         view = auth_view.get_view()
         return view
