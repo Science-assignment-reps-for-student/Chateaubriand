9bdd36b9dbd016f8c61c2b6d50d85930354acc8a
Maly <migsking@naver.com>
2020-07-17 17:14:57 +0900

    [ADD] <config/app_config>
    add the Expire time

--- chateaubriand/config/app_config.py
+++ chateaubriand/config/app_config.py
@@ -1,10 +1,13 @@
 import os
+import datetime
 
 
 class DefaultAppConfig:
     ENV = "development"
     DEBUG = True
     SECRET_KEY = ""
+    ACCESS_TOKEN_EXPIRE_TIME = datetime.timedelta(minutes=10)
+    REFRESH_TOKEN_EXPIRE_TIME = datetime.timedelta(days=14)
 
 
 class DevLevelAppConfig(DefaultAppConfig):
