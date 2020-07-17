069d94ee630d82836c36b8a2008a20008dbfddcd
Maly <migsking@naver.com>
2020-07-17 16:01:26 +0900

    [ADD] <app/controller/admin>
    Blueprint 등록

--- chateaubriand/app/controllers/admin/__init__.py
+++ chateaubriand/app/controllers/admin/__init__.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from flask_restful import Api
 
 from chateaubriand.app.controllers.admin.personal_assignment import PersonalAssignment
+from chateaubriand.app.controllers.admin.account import Account
 
 admin_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 admin_api = Api(admin_blueprint)
 
 admin_api.add_resource(PersonalAssignment, "/personal-assignment")
+admin_api.add_resource(Account, "/account")
