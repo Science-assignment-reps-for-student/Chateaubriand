57170433921b7d6c8f08378fe4654b3619174b1a
Maly <migsking@naver.com>
2020-07-18 14:04:42 +0900

    [ADD] <app/controllers/admin/account.py>
    삭제 컨트롤러

--- chateaubriand/app/controllers/admin/account.py
+++ chateaubriand/app/controllers/admin/account.py
@@ -4,6 +4,7 @@ from flask_restful import Resource
 from chateaubriand.app.services.admin.account import AccountService
 from chateaubriand.app.views.admin.account import AccountView
 from chateaubriand.app.util.json_checker import json_type_validate, POST_ACCOUNT_JSON, DELETE_ACCOUNT_JSON
+from chateaubriand.app.util.token_checker import available_token
 
 class Account(Resource):
     @json_type_validate(POST_ACCOUNT_JSON)
@@ -12,3 +13,14 @@ class Account(Resource):
         account_view = AccountView("POST")
         view = account_view.get_view()
         return view
+
+    @json_type_validate(DELETE_ACCOUNT_JSON)
+    @available_token()
+    def delete(self):
+        AccountService.delete_account(request.json["email"], request.json["password"])
+        account_view = AccountView("DELETE")
+        view = account_view.get_view()
+        return view
+
+
+
