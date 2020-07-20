6e6f8587047df10f428f97feafd877d3cb975316
Maly <migsking@naver.com>
2020-07-20 14:55:42 +0900

    [MODIFY] <test>
    test config로 바꿈

--- test/__init__.py
+++ test/__init__.py
@@ -1,12 +1,12 @@
 import unittest
 
 from chateaubriand.app import create_app
-from chateaubriand.config.redis_config import LocalRedisConfig
-from chateaubriand.config.app_config import DevLevelAppConfig, ProductionLevelAppConfig
-from chateaubriand.config.db_config import LocalDBConfig
+from chateaubriand.config.redis_config import TestRedisConfig
+from chateaubriand.config.app_config import TestLevelAppConfig
+from chateaubriand.config.db_config import TestDBConfig
 
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = create_app(LocalRedisConfig, ProductionLevelAppConfig, LocalDBConfig)
+        self.app = create_app(TestLevelAppConfig, TestDBConfig, TestRedisConfig)
         self.test_client = self.app.test_client()
