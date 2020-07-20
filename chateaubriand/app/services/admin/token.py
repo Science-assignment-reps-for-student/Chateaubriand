a14ad23ae670b5dfbeed0a7f888e63adc8f430c1
Maly <migsking@naver.com>
2020-07-20 13:53:24 +0900

    [CREATE] <app/services/admin/token.py>
    토큰 서비스 추가

---
+++ chateaubriand/app/services/admin/token.py
@@ -0,0 +1,13 @@
+from chateaubriand.app.extensions import redis
+from chateaubriand.app.exception import Unauthorized
+from chateaubriand.app.util.token_generator import decode_token
+
+
+class TokenService:
+    @classmethod
+    def check_token(cls, access_token, refresh_token):
+        redis_db = redis.get_redis()
+        email = decode_token(access_token)
+        saved_refresh_token = redis_db.get(email)
+        if not refresh_token == saved_refresh_token.decode(): raise Unauthorized
+        return email
