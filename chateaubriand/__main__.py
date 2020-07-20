c84eaee1685623865d41fc3b51c854334b182cfb
Maly <migsking@naver.com>
2020-07-20 14:36:09 +0900

    [ADD] <Product Run App>
    Add Redis Config

--- chateaubriand/__main__.py
+++ chateaubriand/__main__.py
@@ -4,8 +4,9 @@ from chateaubriand.app import create_app
 from chateaubriand.const.run_setting import RUN_SETTINGS
 from chateaubriand.config.app_config import ProductionLevelAppConfig
 from chateaubriand.config.db_config import RemoteDBConfig
+from chateaubriand.config.redis_config import RemoteRedisConfig
 
 
 if __name__ == "__main__":
-    app = create_app(ProductionLevelAppConfig, RemoteDBConfig)
+    app = create_app(ProductionLevelAppConfig, RemoteDBConfig, RemoteRedisConfig)
     serve(app, **RUN_SETTINGS)
