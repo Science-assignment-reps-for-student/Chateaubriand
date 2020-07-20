598e909b788a346da2f8ab3fbf93ecaa2a24114d
Maly <migsking@naver.com>
2020-07-20 13:54:01 +0900

    [ADD] <app/controllers/admin>
    토큰 api 등록

--- chateaubriand/app/controllers/admin/__init__.py
+++ chateaubriand/app/controllers/admin/__init__.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from chateaubriand.app.controllers.admin.personal_assignment import PersonalAssignment
 from chateaubriand.app.controllers.admin.account import Account
 from chateaubriand.app.controllers.admin.auth import Auth
+from chateaubriand.app.controllers.admin.token import Token
 
 admin_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 admin_api = Api(admin_blueprint)
@@ -11,3 +12,4 @@ admin_api = Api(admin_blueprint)
 admin_api.add_resource(PersonalAssignment, "/personal-assignment")
 admin_api.add_resource(Account, "/account")
 admin_api.add_resource(Auth, "/auth")
+admin_api.add_resource(Token, "/token")
