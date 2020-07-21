45e88c41f5d3840c8eee42af78ef6768bf39870f
Maly <migsking@naver.com>
2020-07-21 17:18:55 +0900

    [CREATE] <app/controllers/personal..>

--- chateaubriand/app/controllers/admin/personal_assignment.py
+++ chateaubriand/app/controllers/admin/personal_assignment.py
@@ -2,14 +2,14 @@ from flask import request
 from flask_restful import Resource
 
 from chateaubriand.app.views.admin.personal_assignment import PersonalAssignmentView
-from chateaubriand.app.util.json_checker import json_type_validate, GET_ASSIGNMENT_JSON
 from chateaubriand.app.util.token_checker import available_token
-
+from chateaubriand.app.util.param_checker import param_validate, GET_ASSIGNMENT_JSON
 
 class PersonalAssignment(Resource):
-    @json_type_validate(GET_ASSIGNMENT_JSON)
+    @param_validate(GET_ASSIGNMENT_JSON)
     @available_token
     def get(self):
-        personal_assignment_view = PersonalAssignmentView(request.json["class"])
+        print(request.args.keys())
+        personal_assignment_view = PersonalAssignmentView(request.args.get('class'))
         view = personal_assignment_view.get_view()
         return view
