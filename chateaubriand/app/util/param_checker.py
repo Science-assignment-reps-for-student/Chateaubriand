c7c47e039e8e737ac71c54b19e0d70be3030b92d
Maly <migsking@naver.com>
2020-07-21 17:19:35 +0900

    [CREATE] <app/util/param_checker.py>

---
+++ chateaubriand/app/util/param_checker.py
@@ -0,0 +1,23 @@
+from flask import request
+from functools import wraps
+
+from chateaubriand.app.exception import BadRequest
+
+
+def param_validate(schema: list):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            for key in schema:
+                value = request.args.get(key)
+                if value is None:
+                    break
+            else:
+                return fn(*args, **kwargs)
+            raise BadRequest()
+
+        return wrapper
+
+    return decorator
+
+GET_ASSIGNMENT_JSON = ["class"]
