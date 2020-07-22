0d2cefce9f5c4ce4e1081d307a04a0758d8cdb8f
Maly <migsking@naver.com>
2020-07-22 15:56:37 +0900

    [CREATE] <app/models/mutual_evaluation.py>
    상호 평가

---
+++ chateaubriand/app/models/mutual_evaluation.py
@@ -0,0 +1,13 @@
+from chateaubriand.app.extensions import db
+
+
+class MutualEvaluationModel(db.Model):
+    __tablename__ = "mutual_evaluation"
+
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    homework_id = db.Column(db.Integer, db.ForeignKey("homework.id"))
+    target_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    communication = db.Column(db.Integer, nullable=False)
+    cooperation = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
