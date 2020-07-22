b2c71182d0b3a6cd3b891efcd396e6e54b97080f
Maly <migsking@naver.com>
2020-07-22 16:27:41 +0900

    [ADD] <app/models>
    Register models in model init file

--- chateaubriand/app/models/__init__.py
+++ chateaubriand/app/models/__init__.py
@@ -1,3 +1,13 @@
 from chateaubriand.app.models.homework import HomeworkModel
+
+from chateaubriand.app.models.multi_file import MultiFileModel
 from chateaubriand.app.models.single_file import SingleFileModel
+
 from chateaubriand.app.models.student import StudentModel
+from chateaubriand.app.models.admin import AdminModel
+
+from chateaubriand.app.models.member import MemberModel
+from chateaubriand.app.models.team import TeamModel
+
+from chateaubriand.app.models.mutual_evaluation import MutualEvaluationModel
+from chateaubriand.app.models.self_evaluation import SelfEvaluationModel
