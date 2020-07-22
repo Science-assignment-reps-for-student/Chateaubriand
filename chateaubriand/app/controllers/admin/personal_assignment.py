1020987352d15c1f949a5ae171f548e494d33be6
Maly <migsking@naver.com>
2020-07-22 15:59:02 +0900

    [MODIFY] <app/util/param_checker.py>
    파라미터 스키마 이름 변경

--- chateaubriand/app/controllers/admin/personal_assignment.py
+++ chateaubriand/app/controllers/admin/personal_assignment.py
@@ -3,13 +3,12 @@ from flask_restful import Resource
 
 from chateaubriand.app.views.admin.personal_assignment import PersonalAssignmentView
 from chateaubriand.app.util.token_checker import available_token
-from chateaubriand.app.util.param_checker import param_validate, GET_ASSIGNMENT_JSON
+from chateaubriand.app.util.param_checker import param_validate, GET_ASSIGNMENT
 
 class PersonalAssignment(Resource):
-    @param_validate(GET_ASSIGNMENT_JSON)
+    @param_validate(GET_ASSIGNMENT)
     @available_token
     def get(self):
-        print(request.args.keys())
         personal_assignment_view = PersonalAssignmentView(request.args.get('class'))
         view = personal_assignment_view.get_view()
         return view
