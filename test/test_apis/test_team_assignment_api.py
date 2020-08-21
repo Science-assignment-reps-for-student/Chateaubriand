362ed4fb291b96e1d3cbe6f2ab4457049d196101
asdf <migsking@naver.com>
2020-08-22 02:56:29 +0900

    Update Test Case

--- test/test_apis/test_team_assignment_api.py
+++ test/test_apis/test_team_assignment_api.py
@@ -7,10 +7,7 @@ from test.test_apis.mocks import jwt_mock
 class TeamAssignmentTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.path = "/admin/team-assignment"
-        self.common_get = {"class": 1}
-
-        self.invalid_get = {"class": "1"}
+        self.path = "/admin/team-assignment?class=1"
 
         self.common_header = {
             "Authorization": jwt_mock(self.app, "ADMIN", "access_token", bearer=True)
@@ -22,20 +19,12 @@ class TeamAssignmentTestCase(BaseTestCase):
 
     def test_get(self):
         resp_200 = self.test_client.get(
-            self.path, json=self.common_get, headers=self.common_header
+            self.path, headers=self.common_header
         )
-
-        resp_400 = self.test_client.get(
-            self.path, json=self.invalid_get, headers=self.common_header
-        )
-
-        resp_401 = self.test_client.get(self.path, json=self.common_get)
 
         resp_403 = self.test_client.get(
-            self.path, json=self.common_get, headers=self.invalid_header,
+            self.path, headers=self.invalid_header,
         )
 
         self.assertEqual(resp_200.status_code, 200)
-        self.assertEqual(resp_400.status_code, 400)
-        self.assertEqual(resp_401.status_code, 401)
         self.assertEqual(resp_403.status_code, 403)
