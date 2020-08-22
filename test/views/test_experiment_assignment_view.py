01dca54e2b6f5e0f7574098e506fd44c5a4501df
asdf <migsking@naver.com>
2020-08-22 17:05:26 +0900

    [CREATE] <test/views/test_experiment_assignment_view.py>

---
+++ test/views/test_experiment_assignment_view.py
@@ -0,0 +1,47 @@
+from test import BaseTestCase
+
+from unittest.mock import patch
+
+from chateaubriand.app.views.admin.experiment_assignment import ExperimentAssignmentView
+from chateaubriand.app.exception import ViewError
+
+class TestExperimentView(BaseTestCase):
+    def setUp(self):
+        self.experiment_assignment_view_post = ExperimentAssignmentView("1")
+
+    @patch(
+        'chateaubriand.app.views.admin.experiment_assignment.ExperimentAssignmentView.data_merge',
+        return_value = (
+            {
+                "experiment_assignment":  [
+                    {
+                        "id": 1,
+                        "title": "test",
+                        "description": "test",
+                        "created_at": 100000000,
+                        "deadline": 100000000,
+                        "peer_evaluation_submit": [],
+                        "teams_info": [],
+                    }
+                ]
+            }, 200
+        )
+    )
+    def test_post(self, mock_data):
+        view = self.experiment_assignment_view_post.get_view()
+
+        self.assertEqual(view, (
+            {
+                "experiment_assignment":  [
+                    {
+                        "id": 1,
+                        "title": "test",
+                        "description": "test",
+                        "created_at": 100000000,
+                        "deadline": 100000000,
+                        "peer_evaluation_submit": [],
+                        "teams_info": [],
+                    }
+                ]
+            }, 200
+        ))
