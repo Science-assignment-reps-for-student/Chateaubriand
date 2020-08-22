5a18d387dcb47959aed211b70307d6eab7673a9d
asdf <migsking@naver.com>
2020-08-22 17:05:54 +0900

    [CREATE] <test/views/test_team_assignment_view.py>

---
+++ test/views/test_team_assignment_view.py
@@ -0,0 +1,47 @@
+from test import BaseTestCase
+
+from unittest.mock import patch
+
+from chateaubriand.app.views.admin.team_assignment import TeamAssignmentView
+from chateaubriand.app.exception import ViewError
+
+class TestTeamView(BaseTestCase):
+    def setUp(self):
+        self.team_assignment_view_post = TeamAssignmentView("1")
+
+    @patch(
+        'chateaubriand.app.views.admin.team_assignment.TeamAssignmentView.data_merge',
+        return_value = (
+            {
+                "team_assignment": [
+                    {
+                        "id": 1,
+                        "title": "test",
+                        "description": "test",
+                        "created_at": 10000000,
+                        "deadline": 100000000,
+                        "peer_evaluation_submit": [],
+                        "teams_info": []
+                    }
+                ]
+            }, 200
+        )
+    )
+    def test_post(self, mock_data):
+        view = self.team_assignment_view_post.get_view()
+
+        self.assertEqual(view, (
+            {
+                "team_assignment": [
+                    {
+                        "id": 1,
+                        "title": "test",
+                        "description": "test",
+                        "created_at": 10000000,
+                        "deadline": 100000000,
+                        "peer_evaluation_submit": [],
+                        "teams_info": []
+                    }
+                ]
+            }, 200
+        ))
