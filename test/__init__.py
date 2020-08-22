ff79b550b3f93f21bdfc36c28307b0134d8a835d
asdf <migsking@naver.com>
2020-08-22 17:03:43 +0900

    [DELETE] <test, test/__main__>
    테스트 슈트 삭제

--- test/__init__.py
+++ test/__init__.py
@@ -1,12 +1,5 @@
 import unittest
 
-from chateaubriand.app import create_app
-from chateaubriand.config.redis_config import TestRedisConfig
-from chateaubriand.config.app_config import TestLevelAppConfig
-from chateaubriand.config.db_config import TestDBConfig
-
 
 class BaseTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app(TestLevelAppConfig, TestDBConfig, TestRedisConfig)
-        self.test_client = self.app.test_client()
+    pass
