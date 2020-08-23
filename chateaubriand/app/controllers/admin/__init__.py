36fd8c75b2c73623d37f9cabe0350aee1a24a221
asdf <migsking@naver.com>
2020-08-23 19:23:56 +0900

    [MODIFY] <app/controllers/admin> Change prefix

--- chateaubriand/app/controllers/admin/__init__.py
+++ chateaubriand/app/controllers/admin/__init__.py
@@ -10,7 +10,7 @@ from chateaubriand.app.controllers.admin.account import Account
 from chateaubriand.app.controllers.admin.auth import Auth
 from chateaubriand.app.controllers.admin.token import Token
 
-admin_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
+admin_blueprint = Blueprint("chateaubriand", __name__, url_prefix="/v2/chateaubriand")
 admin_api = Api(admin_blueprint)
 
 admin_api.add_resource(PersonalAssignment, "/personal-assignment")
