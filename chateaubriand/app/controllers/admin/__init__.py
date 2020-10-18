8ee3150a8940fc39964c4215965895f21a7c1b71
Maly <migsking@naver.com>
2020-10-18 21:17:36 +0900

    [ADD] Admin controller 등록

--- chateaubriand/app/controllers/admin/__init__.py
+++ chateaubriand/app/controllers/admin/__init__.py
@@ -3,9 +3,8 @@ from flask_restful import Api
 
 from chateaubriand.app.controllers.admin.personal_assignment import PersonalAssignment
 from chateaubriand.app.controllers.admin.team_assignment import TeamAssignment
-from chateaubriand.app.controllers.admin.experiment_assignment import (
-    ExperimentAssignment,
-)
+from chateaubriand.app.controllers.admin.experiment_assignment import ExperimentAssignment
+from chateaubriand.app.controllers.admin.assignment import Assignment
 from chateaubriand.app.controllers.admin.account import Account
 from chateaubriand.app.controllers.admin.auth import Auth
 from chateaubriand.app.controllers.admin.token import Token
@@ -19,3 +18,4 @@ admin_api.add_resource(ExperimentAssignment, "/experiment-assignment")
 admin_api.add_resource(Account, "/account")
 admin_api.add_resource(Auth, "/auth")
 admin_api.add_resource(Token, "/token")
+admin_api.add_resource(Assignment, "/assignment")
